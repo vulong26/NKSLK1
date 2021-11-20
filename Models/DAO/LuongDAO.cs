3726e211120a9d3b7bcbe114bf2e6719e13d40ed
vulong26 <77488668+vulong26@users.noreply.github.com>
2021-11-20 16:54:11 +0700

    a

--- Models/DAO/LuongDAO.cs
+++ Models/DAO/LuongDAO.cs
@@ -31,64 +31,12 @@ namespace NKSLK.Models.DAO
             var lst = db.Database.SqlQuery<CongNhan_Luong>(q).ToPagedList(pageNum, pageSize);
             return lst;
         }
+        public IEnumerable<CongNhan_Luong> LuongCaNhan(int pageNum, int pageSize, string month)
+        {
+            string q = "Select * from luongcongnhan";
+            var lst = db.Database.SqlQuery<CongNhan_Luong>(q).ToPagedList(pageNum, pageSize);
+            return lst;
+        }
 
-        //public void Delete(int id)
-        //{
-        //    NhatKySLK nkk = db.NhatKySLKs.Find(id);
-        //    if (nkk != null)
-        //    {
-        //        db.NhatKySLKs.Remove(nkk);
-        //        db.SaveChanges();
-        //    }
-        //}
-
-        ////public int Create(string name, DateTime? dob, bool gt, string phongban, string chucvu, string quequan, decimal luonghd, decimal luongbh)
-        ////{
-
-        ////    CongNhan cn = new CongNhan();
-        ////    cn.MaCN = db.CongNhans.Max(i => i.MaCN) + 1;
-        ////    cn.HoTen = name;
-        ////    cn.NgaySinh = dob;
-        ////    cn.GioiTinh = gt;
-        ////    cn.PhongBan = phongban;
-        ////    cn.ChucVu = chucvu;
-        ////    cn.QueQuan = quequan;
-        ////    cn.LuongHD = luonghd;
-        ////    cn.LuongBaoHiem = luongbh;
-        ////    db.CongNhans.Add(cn);
-        ////    db.SaveChanges();
-        ////    return cn.MaCN;
-        ////}
-        //public NhatKySLK getById(int id)
-        //{
-        //    return db.NhatKySLKs.Single(i => i.MaNhatKy == id);
-        //}
-        ////public void Edit(CongNhan tmp)
-        ////{
-        ////    CongNhan cn = db.CongNhans.Find(tmp.MaCN);
-        ////    if (cn != null)
-        ////    {
-        ////        cn.HoTen = tmp.HoTen;
-        ////        cn.NgaySinh = tmp.NgaySinh;
-        ////        cn.GioiTinh = tmp.GioiTinh;
-        ////        cn.PhongBan = tmp.PhongBan;
-        ////        cn.ChucVu = tmp.ChucVu;
-        ////        cn.QueQuan = tmp.QueQuan;
-        ////        cn.LuongHD = tmp.LuongHD;
-        ////        cn.LuongBaoHiem = tmp.LuongBaoHiem;
-
-        ////        db.SaveChanges();
-
-        ////    }
-        ////}
-        //public void Add(NhatKySLK nkk)
-        //{
-        //    db.NhatKySLKs.Add(nkk);
-        //    db.SaveChanges();
-        //}
-        ////public Category getCategoryname(Products pro)
-        ////{
-        ////    return db.Category.Single(i => i.ID == pro.IDCATEGORY);
-        ////}
     }
 }
