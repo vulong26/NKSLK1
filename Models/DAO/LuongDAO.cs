bd3eedf7524cbd12aac0c6ca6286c6117665d6ed
Huy <ngotronghuy2000@gmail.com>
2021-11-20 16:52:29 +0700

    luongmaxmin

--- Models/DAO/LuongDAO.cs
+++ Models/DAO/LuongDAO.cs
@@ -32,63 +32,35 @@ namespace NKSLK.Models.DAO
             return lst;
         }
 
-        //public void Delete(int id)
-        //{
-        //    NhatKySLK nkk = db.NhatKySLKs.Find(id);
-        //    if (nkk != null)
-        //    {
-        //        db.NhatKySLKs.Remove(nkk);
-        //        db.SaveChanges();
-        //    }
-        //}
-
-        ////public int Create(string name, DateTime? dob, bool gt, string phongban, string chucvu, string quequan, decimal luonghd, decimal luongbh)
-        ////{
-
-        ////    CongNhan cn = new CongNhan();
-        ////    cn.MaCN = db.CongNhans.Max(i => i.MaCN) + 1;
-        ////    cn.HoTen = name;
-        ////    cn.NgaySinh = dob;
-        ////    cn.GioiTinh = gt;
-        ////    cn.PhongBan = phongban;
-        ////    cn.ChucVu = chucvu;
-        ////    cn.QueQuan = quequan;
-        ////    cn.LuongHD = luonghd;
-        ////    cn.LuongBaoHiem = luongbh;
-        ////    db.CongNhans.Add(cn);
-        ////    db.SaveChanges();
-        ////    return cn.MaCN;
-        ////}
-        //public NhatKySLK getById(int id)
-        //{
-        //    return db.NhatKySLKs.Single(i => i.MaNhatKy == id);
-        //}
-        ////public void Edit(CongNhan tmp)
-        ////{
-        ////    CongNhan cn = db.CongNhans.Find(tmp.MaCN);
-        ////    if (cn != null)
-        ////    {
-        ////        cn.HoTen = tmp.HoTen;
-        ////        cn.NgaySinh = tmp.NgaySinh;
-        ////        cn.GioiTinh = tmp.GioiTinh;
-        ////        cn.PhongBan = tmp.PhongBan;
-        ////        cn.ChucVu = tmp.ChucVu;
-        ////        cn.QueQuan = tmp.QueQuan;
-        ////        cn.LuongHD = tmp.LuongHD;
-        ////        cn.LuongBaoHiem = tmp.LuongBaoHiem;
-
-        ////        db.SaveChanges();
-
-        ////    }
-        ////}
-        //public void Add(NhatKySLK nkk)
-        //{
-        //    db.NhatKySLKs.Add(nkk);
-        //    db.SaveChanges();
-        //}
-        ////public Category getCategoryname(Products pro)
-        ////{
-        ////    return db.Category.Single(i => i.ID == pro.IDCATEGORY);
-        ////}
+        public IEnumerable<CongNhan_Luong> listLuong_CaNhan(int pageNum, int pageSize)
+        {
+            string q = "SELECT * FROM dbo.v_LuongCongNhan";
+            var lst = db.Database.SqlQuery<CongNhan_Luong>(q).ToPagedList(pageNum, pageSize);
+            return lst;
+        }
+        public IEnumerable<CongNhan_Luong> listSortMax_LuongCaNhan(int pageNum, int pageSize)
+        {
+            string q = "SELECT * FROM dbo.v_LuongCongNhan ORDER BY Luong DESC";
+            var lst = db.Database.SqlQuery<CongNhan_Luong>(q).ToPagedList(pageNum, pageSize);
+            return lst;
+        }
+        public IEnumerable<CongNhan_Luong> listSortMin_LuongCaNhan(int pageNum, int pageSize)
+        {
+            string q = "SELECT * FROM dbo.v_LuongCongNhan ORDER BY Luong ASC";
+            var lst = db.Database.SqlQuery<CongNhan_Luong>(q).ToPagedList(pageNum, pageSize);
+            return lst;
+        }
+        public IEnumerable<CongNhan_Luong> listMax_LuongCaNhan(int pageNum, int pageSize)
+        {
+            string q = "SELECT MAX([Luong]) AS 'Max' FROM dbo.v_LuongCongNhan";
+            var lst = db.Database.SqlQuery<CongNhan_Luong>(q).ToPagedList(pageNum, pageSize);
+            return lst;
+        }
+        public IEnumerable<CongNhan_Luong> listMin_LuongCaNhan(int pageNum, int pageSize)
+        {
+            string q = "SELECT * FROM dbo.v_LuongCongNhan";
+            var lst = db.Database.SqlQuery<CongNhan_Luong>(q).ToPagedList(pageNum, pageSize);
+            return lst;
+        }
     }
 }
